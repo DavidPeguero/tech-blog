c481c0a2e014a628d6e5bd3dff0a79a186e8a770
David Peguero <david.peguero.cancel@gmail.com>
2023-05-07 10:17:22 -0400

    Altered some routes for post and created the main layout

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.get('/', async (req,res) => {
+    try{
+      res.render('homepage')
+    } catch(err){
+      res.status(400).json(err)
+    }
+  })
+
 module.exports = router;
